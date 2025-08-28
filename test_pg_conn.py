f0fa39b9699b1ea0e1619cc5345a819e5b095511
Rttintas <rtmuitomais@gmail.com>
2025-08-28 18:49:19 -0300

    Limpa repo: mantém só app e configs para o Render

--- test_pg_conn.py
+++
@@ -1,9 +0,0 @@
-import os, psycopg2
-dsn = os.environ.get("PG_DSN")
-print("[INFO] PG_DSN =", dsn)
-try:
-    with psycopg2.connect(dsn) as conn, conn.cursor() as cur:
-        cur.execute("SELECT version();")
-        print("[OK] Conectou! ->", cur.fetchone()[0])
-except Exception as e:
-    print("[ERRO] Falhou conexão:", e)
--- teste_env.py
+++
@@ -1,9 +0,0 @@
-# teste_env.py
-import os
-from dotenv import load_dotenv
-
-# carrega .env do diretório config
-load_dotenv(r"G:\Meu Drive\AutomacoesOmie\config\.env")
-
-vars_ = ["DB_HOST","DB_PORT","DB_NAME","DB_USER","DB_SCHEMA"]
-print({k: os.getenv(k) for k in vars_})
